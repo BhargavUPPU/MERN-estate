26cbb479635bdca09ba172ab9c3e82b81ab28517
BhargavUPPU <uppubhargavsai2013@gmail.com>
2023-10-08 17:08:03 +0530

    connect to the databases

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+mongoose.connect(process.env.Mongo).then(()=>{
+    console.log("connected to the MongoDb")
+})
+.catch((err)=>{
+    console.log(err);
+});
 const app=express();
 app.listen(3000,()=>{
     console.log('server is running on prot 3000')
