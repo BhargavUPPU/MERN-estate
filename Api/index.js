fea49b314982dd15640bc00e402864c7eac47b67
BhargavUPPU <uppubhargavsai2013@gmail.com>
2023-10-08 22:02:19 +0530

    create sign up route

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.Mongo).then(()=>{
     console.log("connected to the MongoDb")
@@ -13,4 +14,6 @@ const app=express();
 app.listen(3000,()=>{
     console.log('server is running on prot 3000')
 });
+app.use(express.json());
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
