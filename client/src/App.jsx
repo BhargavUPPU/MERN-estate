3d5c35530f3db1acc852d79bb06213ae38e7742b
BhargavUPPU <uppubhargavsai2013@gmail.com>
2023-10-15 10:56:48 +0530

    complete Profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 export default function App() {
   return <BrowserRouter>
   <Header/>
@@ -15,6 +16,9 @@ export default function App() {
   <Route path='/profile'element={<Profile/>}/>
     <Route path='/sign-up'element={<SignUp/>}/>
     <Route path='/about'element={<About/>}/>
+    <Route element={<PrivateRoute/>}>
+      <Route path='/profile' element={<Profile/>}/>
+    </Route>
   </Routes>
   </BrowserRouter>
 }
